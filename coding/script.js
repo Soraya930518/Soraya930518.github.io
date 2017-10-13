6c19f54d5009b7e921be2ab7d479ea48f586d64b
Soraya930518 <32701710+Soraya930518@users.noreply.github.com>
2017-10-13 11:36:30 +0200

    Add files via upload

---
+++ coding/script.js
@@ -0,0 +1,19 @@
+console.log("hello"); 
+console.log(2+5);
+console.log(Math.random()*100);
+if (5>4) {
+	console.log("it is true");
+}
+else { 
+	console.log("it is false");
+ }
+
+ function printApples(unknown) {
+ 	while (unknown<15) {
+ 		console.log("I Have an apple"); 
+ 		unknown+=1; 
+ 	}
+
+ }
+ printApples(10); 
+document.getElementById("pop").innerHTML = ""; 
